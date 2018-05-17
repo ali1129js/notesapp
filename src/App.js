ad4e74b682e4f221f967e098fe67b37db2eba60a
ali1129js <ali1129js@gmail.com>
2018-05-17 17:18:54 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+/**
+ * @Author: Ali
+ * @Date:   2018-05-17T17:18:03+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-05-17T17:18:41+02:00
+ */
+import React, { Component } from 'react'
 import './App.css';
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/**
+ * @Author: Ali
+ * @Date:   2018-05-17T17:18:29+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-05-17T17:18:34+02:00
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
