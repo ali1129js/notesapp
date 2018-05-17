9862df850ea234d9a15177f10a3a4edbf355484e
ali1129js <ali1129js@gmail.com>
2018-05-17 21:30:27 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@
  * @Author: Ali
  * @Date:   2018-05-17T17:18:03+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-17T21:29:51+02:00
+ * @Last modified time: 2018-05-17T21:30:17+02:00
  */
 import React, { Component } from 'react'
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+
       </div>
     );
   }
 }
 
-export default App;
+export default App
